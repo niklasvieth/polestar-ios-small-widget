707e2bd966cd44af703ccf122a5ee673e27c4917
Niklas Vieth <niklasvieth@web.de>
2024-04-13 22:09:02 +0200

    feature: initial setup

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# polestar-ios-small-widget
-Polestar iOS small widget via Scriptable
+# Polestar SoC iOS Small Widget
+
+An iOS small widget to display the current state of charge (SoC) of your Polestar.
+
+
+<img src="./images/polestar_small_widget.png" width="300" alt="Polestar Small Widget"/>
+
+## Background
+
+Since the official Polestar App does not support any kind of widgets for iOS, I developed my own with the help of [Scriptable](https://scriptable.app/). It uses the official Polestar API which is also used to display the battery status in your profile at https://www.polestar.com.
+
+> This widget is a hobby project and the developed code is not affiliated with Polestar AB or any Volvo company or any subsidiary or affiliate of any of the aforementioned companies.
+
+## Prerequisites
+
+1. Polestar Account login credentials which should work at https://polestarid.eu.polestar.com/PolestarLogin/ (optionally the VIN of your car if more than one car is linked to the account).
+2. Install [Scriptable](https://apps.apple.com/de/app/scriptable/id1405459188) on your iPhone
+3. [Optional but recommended] Follow the [ScriptDude installation steps](https://scriptdu.de/#installation)
+
+## Installation
+
+1. Make sure you have followed all mandatory steps described in [Prerequisites](./README.md#Prerequisites).
+
+2. There are two options to install the widget:
+    - [Recommended] Click on the following button to install via `ScriptDude` (make sure that you have followed step 3 of [Prerequisites](./README.md#Prerequisites)). ScriptDude will automatically receive updates of new versions of this widget
+
+        [![Download with ScriptDude](https://scriptdu.de/download.svg)](https://scriptdu.de?name=Polestar%20Small%20SoC%20Widget&source=https%3A%2F%2Fgist.githubusercontent.com%2Fniklasvieth%2Fa45f6e33c117421deb4611e73ad83651%2Fraw%2Fb959e6a0bc2d31b2b455dd5543daf5c458a2d275%2Fpolestar-small-widget.js&docs=https%3A%2F%2Fgithub.com%2Fniklasvieth%2Fpolestar-ios-small-widget%2Fblob%2Fmain%2FREADME.md)
+
+    - Copy the content of [polestar-small-widget.js](https://gist.github.com/niklasvieth/159c13dd7ef94bd608358ce964b66c7c), create a new script in `Scriptable`, paste the content and rename it to `Polestar Small SoC Widget`.
+
+3. Replace the placeholder values for `POLESTAR_EMAIL` and `POLESTAR_PASSWORD` with your Polestar login credentials.
+
+    ```js
+    // Mandatory Config
+    const POLESTAR_EMAIL = "EMAIL";
+    const POLESTAR_PASSWORD = "PASSWORD";
+    ```
+
+4. [Optional Configuration] You can decide if you prefer the relative live counter or the absolute timestamp for the last seen date in the widget footer. Default is the relative format. Change the value to `false` if you prefer the absolute date. You can additionally choose the angle of the image to be shown in the widget, change the battery color threshold values and enable miles instead of kilometer values.  
+
+    ```js
+    const IMAGE_ANGLE = "0"; // Possible values 0,1,2,3,4,5
+    const RANGE_IN_MILES = false; // true
+    const LAST_SEEN_RELATIVE_DATE = false; // true
+
+    const MIN_SOC_GREEN = 60;
+    const MIN_SOC_ORANGE = 30;
+    ```
+
+5. Add the small `Scriptable` widget to your homescreen. See [Apple How-To guide](https://support.apple.com/en-us/HT207122#:~:text=How%20to%20add%20widgets%20to%20your%20Home%20Screen).
+
+    <img src="./images/scriptable_small_widget.jpeg" width="200" alt="Scriptable Small Widget"/>
+
+6. Tab on the widget to configure the widget. Select `Polestar Small SoC Widget` as script, leave everything else blank.
+
+7. You should now be able to see the :battery: SoC of your Polestar on you homescreen. By clicking on the widget you will be navigated to the Polestar App.
+
+8. [Optional] Mac users: You can even add the widget to you your Desktop or Notification Center if you have followed the above steps on you iPhone (see [Apple How-To Guide](https://support.apple.com/en-al/guide/mac-help/mchl52be5da5/mac#:~:text=Use%20iPhone%20widgets%20on%20your%20Mac)).
+
+## Additional comments
+
+There might be a minor lag or difference of the SoC compared to value displayed in the Polestar App because of the following reason:
+
+- The refresh interval of the widget is determined by iOS itself.
+
+The widget supports light and dark mode. The mode is automatically fetched from your settings. If you want to enable or disable dark mode via
+
+```js
+const DARK_MODE = true // false
+```
+
+### Appreciation
+
+- Thanks @simonbs for the awesome [Scriptable](https://scriptable.app/) app.
+
+### Support me
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/niklasvieth)
+
+<a href="https://www.paypal.me/niklasvieth" >
+  <img src="https://raw.githubusercontent.com/stefan-niedermann/paypal-donate-button/master/paypal-donate-button.png" alt="Donate with PayPal" width="200px" />
+</a>
---
+++ polestar-small-widget.js
@@ -0,0 +1,420 @@
+// icon-color: green; icon-glyph: battery-half;
+
+/**
+ * This widget has been developed by Niklas Vieth.
+ * Installation and configuration details can be found at https://github.com/niklasvieth/polestar-ios-small-widget
+ */
+
+// Mandatory Config
+const POLESTAR_EMAIL = "EMAIL";
+const POLESTAR_PASSWORD = "PASSWORD";
+// Optional (only necessary if more than one car linked to account)
+let VIN;
+// let VIN = "VIN";
+let VEHICLE_NAME;
+// let VEHICLE_NAME = "Polestar Custom Name";
+
+// Additional optional configuration
+const IMAGE_ANGLE = "5"; // Possible values 0,1,2,3,4,5
+const RANGE_IN_MILES = false; // true
+const LAST_SEEN_RELATIVE_DATE = false; // true
+const MIN_SOC_GREEN = 60;
+const MIN_SOC_ORANGE = 30;
+
+const DARK_MODE = Device.isUsingDarkAppearance(); // or set manually to (true or false)
+const DARK_BG_COLOR = "000000";
+const LIGHT_BG_COLOR = "FFFFFF";
+
+// API config
+const POLESTAR_BASE_URL = "https://pc-api.polestar.com/eu-north-1";
+const POLESTAR_API_URL_V2 = `${POLESTAR_BASE_URL}/mystar-v2`;
+const POLESTAR_API_URL = `${POLESTAR_BASE_URL}/my-star`;
+const POLESTAR_ICON = "https://www.polestar.com/w3-assets/coast-228x228.png";
+
+// Check that params are set
+if (POLESTAR_EMAIL === "EMAIL") {
+  throw new Error("POLESTAR_EMAIL is not configured");
+}
+if (POLESTAR_PASSWORD === "PASSWORD") {
+  throw new Error("POLESTAR_PASSWORD is not configured");
+}
+if (VIN === "VIN") {
+  throw new Error("VIN is not configured");
+}
+
+// Create Widget
+const accessToken = await getAccessToken();
+const vehicleData = await getVehicles(accessToken);
+const [batteryData, odometerData] = await Promise.all([
+  getBattery(accessToken),
+  getOdometerData(accessToken),
+]);
+
+// You can run the script in the app to preview the widget or you can go to the Home Screen, add a new Scriptable widget and configure the widget to run this script.
+// You can also try creating a shortcut that runs this script. Running the shortcut will show widget.
+const widget = await createPolestarWidget(
+  batteryData,
+  odometerData,
+  vehicleData
+);
+if (config.runsInWidget) {
+  // The script runs inside a widget, so we pass our instance of ListWidget to be shown inside the widget on the Home Screen.
+  Script.setWidget(widget);
+} else {
+  // The script runs inside the app, so we preview the widget.
+  widget.presentSmall();
+}
+
+// Calling Script.complete() signals to Scriptable that the script have finished running.
+// This can speed up the execution, in particular when running the script from Shortcuts or using Siri.
+Script.complete();
+
+// Create polestar widget
+async function createPolestarWidget(batteryData, odometerData, vehicle) {
+  const batteryPercent = parseInt(batteryData.batteryChargeLevelPercentage);
+  const isCharging = batteryData.chargingStatus === "CHARGING_STATUS_CHARGING";
+  const remainingChargingTime = batteryData.estimatedChargingTimeToFullMinutes;
+  const rangeKm = batteryData.estimatedDistanceToEmptyKm;
+  const rangeMiles = batteryData.estimatedDistanceToEmptyMiles;
+  const isChargingDone = batteryData.chargingStatus === "CHARGING_STATUS_DONE";
+  const isConnected =
+    batteryData.chargerConnectionStatus ===
+    "CHARGER_CONNECTION_STATUS_CONNECTED";
+
+  // Prepare image
+  if (!vehicle.content.images.studio.angles.includes(IMAGE_ANGLE)) {
+    throw new Error(
+      `IMG_ANGLE ${IMAGE_ANGLE} is not in ${vehicle.content.images.studio.angles}`
+    );
+  }
+  const imgUrl = `${
+    vehicle.content.images.studio.url
+  }&angle=${IMAGE_ANGLE}&bg=${
+    DARK_MODE ? DARK_BG_COLOR : LIGHT_BG_COLOR
+  }&width=600`;
+
+  const appIcon = await loadImage(POLESTAR_ICON);
+  const title = VEHICLE_NAME ?? vehicle.content.model.name;
+  const widget = new ListWidget();
+  widget.url = "polestar-explore://";
+  const mainStack = widget.addStack();
+  mainStack.layoutVertically();
+
+  // Add background color
+  widget.backgroundColor = DARK_MODE
+    ? new Color(DARK_BG_COLOR)
+    : new Color(LIGHT_BG_COLOR);
+
+  // Show app icon and title
+  mainStack.addSpacer();
+  const titleStack = mainStack.addStack();
+  const titleElement = titleStack.addText(title);
+  titleElement.textColor = DARK_MODE ? Color.white() : Color.black();
+  titleElement.textOpacity = 0.7;
+  titleElement.font = Font.mediumSystemFont(14);
+  titleStack.addSpacer();
+  const appIconElement = titleStack.addImage(appIcon);
+  appIconElement.imageSize = new Size(18, 18);
+  appIconElement.cornerRadius = 4;
+  mainStack.addSpacer();
+
+  // Battery Info
+  const batteryInfoStack = mainStack.addStack();
+  batteryInfoStack.layoutVertically();
+
+  // Battery Percent Value
+  const batteryPercentStack = batteryInfoStack.addStack();
+  batteryPercentStack.centerAlignContent();
+  const { batteryIcon, batteryIconColor } = getBatteryIcon(
+    batteryPercent,
+    isConnected,
+    isCharging,
+    isChargingDone
+  );
+  const batterySymbolElement = batteryPercentStack.addImage(batteryIcon.image);
+  batterySymbolElement.imageSize = new Size(15, 15);
+  batterySymbolElement.tintColor = batteryIconColor;
+  batteryPercentStack.addSpacer(4);
+
+  const batteryPercentText = batteryPercentStack.addText(`${batteryPercent} %`);
+  batteryPercentText.textColor = getBatteryPercentColor(batteryPercent);
+  batteryPercentText.font = Font.boldSystemFont(14);
+  batteryPercentStack.addSpacer();
+
+  // Range
+  const rangeText = RANGE_IN_MILES ? `${rangeMiles} mi` : `${rangeKm} km`;
+  const rangeElement = batteryPercentStack.addText(rangeText);
+  rangeElement.font = Font.mediumSystemFont(12);
+  rangeElement.textColor = DARK_MODE ? Color.white() : Color.black();
+  rangeElement.rightAlignText();
+
+  if (isCharging) {
+    batteryInfoStack.addSpacer(2);
+    const batteryChargingTimeStack = batteryInfoStack.addStack();
+    const remainingChargeTimeHours = parseInt(remainingChargingTime / 60);
+    const remainingChargeTimeMinsRemainder = remainingChargingTime % 60;
+    const chargingTimeElement = batteryChargingTimeStack.addText(
+      `${remainingChargeTimeHours}h ${remainingChargeTimeMinsRemainder}m`
+    );
+    chargingTimeElement.font = Font.mediumSystemFont(10);
+    chargingTimeElement.textColor = DARK_MODE ? Color.white() : Color.black();
+    chargingTimeElement.rightAlignText();
+  }
+  mainStack.addSpacer();
+
+  // Image
+  const imageStack = mainStack.addStack();
+  imageStack.addSpacer();
+  const carImage = await loadImage(imgUrl);
+  const carImageElement = imageStack.addImage(carImage);
+  carImageElement.imageSize = new Size(100, 50);
+  imageStack.addSpacer();
+  mainStack.addSpacer(4);
+
+  // Add odometer
+  const odometerText = RANGE_IN_MILES
+    ? `${parseInt(odometerData.odometerMeters / 1609.344).toLocaleString()} mi`
+    : `${parseInt(odometerData.odometerMeters / 1000).toLocaleString()} km`;
+  const odometerElement = mainStack.addText(odometerText);
+  odometerElement.font = Font.mediumSystemFont(10);
+  odometerElement.textColor = DARK_MODE ? Color.white() : Color.black();
+  odometerElement.textOpacity = 0.7;
+  mainStack.addSpacer(2);
+
+  // Add last seen indicator
+  const footerStack = mainStack.addStack();
+  footerStack.addSpacer();
+  const lastSeenDate = new Date(batteryData.eventUpdatedTimestamp.iso);
+  const lastSeenText = lastSeenDate.toLocaleString();
+  let lastSeenElement;
+  if (LAST_SEEN_RELATIVE_DATE) {
+    lastSeenElement = footerStack.addDate(lastSeenDate);
+    lastSeenElement.applyRelativeStyle();
+  } else {
+    lastSeenElement = footerStack.addText(lastSeenText);
+  }
+  lastSeenElement.font = Font.mediumSystemFont(10);
+  lastSeenElement.textOpacity = 0.5;
+  lastSeenElement.textColor = DARK_MODE ? Color.white() : Color.black();
+  lastSeenElement.minimumScaleFactor = 0.7;
+
+  return widget;
+}
+
+/**********************
+ * Polestar API helpers
+ **********************/
+async function getAccessToken() {
+  const { pathToken, cookie } = await getLoginFlowTokens();
+  const tokenRequestCode = await performLogin(pathToken, cookie);
+  const apiCreds = await getApiToken(tokenRequestCode);
+  return apiCreds.access_token;
+}
+
+async function performLogin(pathToken, cookie) {
+  const req = new Request(
+    `https://polestarid.eu.polestar.com/as/${pathToken}/resume/as/authorization.ping`
+  );
+  req.method = "post";
+  req.body = getUrlEncodedParams({
+    "pf.username": POLESTAR_EMAIL,
+    "pf.pass": POLESTAR_PASSWORD,
+  });
+  req.headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Cookie: cookie,
+  };
+  req.onRedirect = (redReq) => {
+    return null;
+  };
+  await req.load();
+  const redirectUrl = req.response.headers.Location;
+  const regex = /code=([^&]+)/;
+  const match = redirectUrl.match(regex);
+  const tokenRequestCode = match ? match[1] : null;
+  return tokenRequestCode;
+}
+
+async function getLoginFlowTokens() {
+  const req = new Request(
+    "https://polestarid.eu.polestar.com/as/authorization.oauth2?response_type=code&client_id=polmystar&redirect_uri=https://www.polestar.com%2Fsign-in-callback&scope=openid+profile+email+customer%3Aattributes"
+  );
+  req.headers = { Cookie: "" };
+  let redirectUrl;
+  req.onRedirect = (redReq) => {
+    redirectUrl = redReq.url;
+    return null;
+  };
+  await req.loadString();
+  const regex = /resumePath=(\w+)/;
+  const match = redirectUrl.match(regex);
+  const pathToken = match ? match[1] : null;
+  const cookies = req.response.headers["Set-Cookie"];
+  const cookie = cookies.split("; ")[0] + ";";
+  return {
+    pathToken: pathToken,
+    cookie: cookie,
+  };
+}
+
+async function getApiToken(tokenRequestCode) {
+  const req = new Request(`${POLESTAR_BASE_URL}/auth`);
+  req.method = "POST";
+  req.headers = {
+    "Content-Type": "application/json",
+  };
+  req.body = JSON.stringify({
+    query:
+      "query getAuthToken($code: String!){getAuthToken(code: $code){id_token,access_token,refresh_token,expires_in}}",
+    operationName: "getAuthToken",
+    variables: { code: tokenRequestCode },
+  });
+  req.onRedirect = (redReq) => {
+    return null;
+  };
+  const response = await req.loadJSON();
+  const apiCreds = response.data.getAuthToken;
+  return {
+    access_token: apiCreds.access_token,
+    refresh_token: apiCreds.refresh_token,
+    expires_in: apiCreds.expires_in,
+  };
+}
+
+async function getVehicles(accessToken) {
+  if (!accessToken) {
+    throw new Error("Not authenticated");
+  }
+  const searchParams = {
+    query:
+      "query getCars{getConsumerCarsV2{vin,internalVehicleIdentifier,modelYear,content{model{code,name},images,{studio,{url,angles}}},hasPerformancePackage,registrationNo,deliveryDate,currentPlannedDeliveryDate}}",
+    variables: {},
+  };
+  const req = new Request(POLESTAR_API_URL);
+  req.method = "POST";
+  req.headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + accessToken,
+  };
+  req.body = JSON.stringify(searchParams);
+  const response = await req.loadJSON();
+  const vehicleData = response?.data?.getConsumerCarsV2;
+  if (!vehicleData) {
+    throw new Error("No vehicle data fetched");
+  }
+  const vehicle =
+    vehicleData.find((vehicle) => vehicle.vin === VIN) ?? vehicleData[0];
+  if (!vehicle) {
+    throw new Error(`No vehicle found with VIN ${VIN}`);
+  }
+  VIN = vehicle.vin;
+  return vehicle;
+}
+
+async function getBattery(accessToken) {
+  if (!accessToken) {
+    throw new Error("Not authenticated");
+  }
+  const searchParams = {
+    query:
+      "query GetBatteryData($vin:String!){getBatteryData(vin:$vin){averageEnergyConsumptionKwhPer100Km,batteryChargeLevelPercentage,chargerConnectionStatus,chargingCurrentAmps,chargingPowerWatts,chargingStatus,estimatedChargingTimeMinutesToTargetDistance,estimatedChargingTimeToFullMinutes,estimatedDistanceToEmptyKm,estimatedDistanceToEmptyMiles,eventUpdatedTimestamp{iso,unix}}}",
+    variables: {
+      vin: VIN,
+    },
+  };
+  const req = new Request(POLESTAR_API_URL_V2);
+  req.method = "POST";
+  req.headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + accessToken,
+  };
+  req.body = JSON.stringify(searchParams);
+  const response = await req.loadJSON();
+  if (!response?.data?.getBatteryData) {
+    throw new Error("No battery data fetched");
+  }
+  return response.data.getBatteryData;
+}
+
+async function getOdometerData(accessToken) {
+  if (!accessToken) {
+    throw new Error("Not authenticated");
+  }
+  const searchParams = {
+    query:
+      "query GetOdometerData($vin: String!){getOdometerData(vin:$vin){averageSpeedKmPerHour,eventUpdatedTimestamp,{iso,unix},odometerMeters,tripMeterAutomaticKm,tripMeterManualKm}}",
+    variables: {
+      vin: VIN,
+    },
+  };
+  const req = new Request(POLESTAR_API_URL_V2);
+  req.method = "POST";
+  req.headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + accessToken,
+  };
+  req.body = JSON.stringify(searchParams);
+  const response = await req.loadJSON();
+  if (!response?.data?.getOdometerData) {
+    throw new Error("No odometer data fetched");
+  }
+  return response.data.getOdometerData;
+}
+
+async function loadImage(url) {
+  const req = new Request(url);
+  return req.loadImage();
+}
+
+function getUrlEncodedParams(object) {
+  return Object.keys(object)
+    .map((key) => `${key}=${encodeURIComponent(object[key])}`)
+    .join("&");
+}
+
+/*************
+ * Formatters
+ *************/
+function getBatteryPercentColor(percent) {
+  if (percent > MIN_SOC_GREEN) {
+    return Color.green();
+  } else if (percent > MIN_SOC_ORANGE) {
+    return Color.orange();
+  } else {
+    return Color.red();
+  }
+}
+
+function getBatteryIcon(
+  batteryPercent,
+  isConnected,
+  isCharging,
+  isChargingDone
+) {
+  let icon;
+  let iconColor;
+  if (isCharging || isChargingDone) {
+    icon = isCharging
+      ? SFSymbol.named("bolt.fill")
+      : SFSymbol.named("bolt.badge.checkmark.fill");
+    iconColor = Color.green();
+  } else if (isConnected) {
+    icon = SFSymbol.named("bolt.badge.xmark");
+    iconColor = Color.red();
+  } else {
+    let percentRounded = 0;
+    iconColor = Color.red();
+    if (batteryPercent > 90) {
+      percentRounded = 100;
+    } else if (batteryPercent > 60) {
+      percentRounded = 75;
+    } else if (batteryPercent > 40) {
+      percentRounded = 50;
+    } else if (batteryPercent > 10) {
+      percentRounded = 25;
+    }
+    iconColor = getBatteryPercentColor(batteryPercent);
+    icon = SFSymbol.named(`battery.${percentRounded}`);
+  }
+  return { batteryIcon: icon, batteryIconColor: iconColor };
+}
